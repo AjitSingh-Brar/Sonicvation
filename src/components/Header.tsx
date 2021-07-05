b178d236a7fbd8fe3efa718caad02f8182207ff4
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-07-05 18:11:34 -0400

    ADD: Header component and Home Component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      <div className="headerLogo">
+        <img src="../images/logo_app.png" alt="image-logo" />
+      </div>
+      <div className="headerLogin">
+        <div className="headerLoginButton">LOGIN</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="homeContainer">Hello</div>
+    </div>
+  );
+}
+
+export default Home;
