12e8a94f5b442f0e6f9545d3e0c077172abb52ef
Ajit Singh <68308589+AjitSingh-Brar@users.noreply.github.com>
2021-07-05 20:01:36 -0400

    ADD: Login component and Dashboard component prepared

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,23 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <Router>
         <Header />
-        <Home />
-      </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Login() {
+  return <div className="login">Hello</div>;
+}
+
+export default Login;
